b241c50f87b1e3ce547503004102646847b1a549
Nicolás Gómez <nicolas@mtblc.co>
2019-05-02 15:30:38 -0400

    revert changes in pay.js

--- pay.js
+++ pay.js
@@ -1,16 +1,5 @@
 const pay = require('donna-bot/services/sii/f29')
 
-const monthlyCheck = require('./monthlyCheck')
-
-function logSuccess () {
-  console.info('Success!!')
-}
-
-function logError (e) {
-  console.log('Something went wrong')
-  console.error(e)
-}
-
 function run () {
   const { PASSWORD, RUT } = process.env
 
@@ -24,27 +13,9 @@ function run () {
 
   console.info('Running pay SII function')
 
-  const today = new Date()
-  const month = today.getMonth()
-  const year = today.getFullYear()
-
   pay(RUT, PASSWORD)
-    .then(() => console.log('Success!') && process.exit())
-    .catch(e => logError(e) && process.exit(1))
-
-  /* return monthlyCheck
-    .exists({ month, year })
-    .then(exists => {
-      if (exists) {
-        console.info(`Already called for month = ${month} and year = ${year}`)
-      } else {
-        return pay(RUT, PASSWORD).then(
-          () => logSuccess() && monthlyCheck.insert({ month, year })
-        )
-      }
-    })
-    .then(() => process.exit())
-    .catch(e => logError(e) && process.exit(1)) */
+    .then(() => console.info('Success'))
+    .catch(() => console.error('Something went wrong'))
 }
 
 run()
