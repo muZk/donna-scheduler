a0510d0d4e286a0bc813c5b89ca2d917691f07cc
Nicolás Gómez <nicolas@mtblc.co>
2019-05-31 18:41:19 -0400

    :sparkles: fixes db queries and adds telegram bot :robot:

--- pay.js
+++ pay.js
@@ -1,6 +1,14 @@
 const pay = require('donna-bot/services/sii/f29')
+const db = require('monk')(process.env.MONGODB_URI)
+const telegram = require('telegram-bot-api')
 
-function run () {
+const logs = db.get('logs')
+
+const bot = new telegram({
+  token: process.env.BOT_TOKEN
+})
+
+async function run () {
   const { PASSWORD, RUT } = process.env
 
   if (!PASSWORD) {
@@ -11,11 +19,31 @@ function run () {
     return console.error('RUT is missing from ENV variables')
   }
 
-  console.info('Running pay SII function')
+  const now = new Date()
+  const currentMonth = { month: now.getMonth() + 1, year: now.getFullYear() }
 
-  pay(RUT, PASSWORD)
-    .then(() => console.info('Success'))
-    .catch(() => console.error('Something went wrong'))
+  const log = await logs.findOne(currentMonth)
+
+  if (log) {
+    console.info('Already run for this month')
+    return
+  }
+
+  try {
+    // await pay(RUT, PASSWORD)
+    console.info('Success')
+    bot.sendMessage({
+      chat_id: process.env.BOT_CHAT_ID,
+      text: 'Listo! Tus impuestos del SII están pagados 🎉🎉🎉🎉'
+    })
+    await logs.insert(currentMonth)
+  } catch (e) {
+    console.error('Something went wrong')
+    throw e
+  }
 }
 
 run()
+  .then(() => console.log('Program finished successfully'))
+  .catch(e => console.error('There was an error', e))
+  .then(() => db.close())
