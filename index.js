ee81fe37ed04d3ec28f6111497dbb81e9af62314
Nicolás Gómez <nicolas@mtblc.co>
2019-06-06 16:21:30 -0400

    :sparkles: redirect to GitHub repo

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .get('/', (req, res) => res.render('Hello there!'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) =>
+    res.redirect('https://github.com/muZk/donna-scheduler')
+  )
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
